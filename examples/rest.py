4b75181c4514c2130173ee1f49760a19654384bd
Awethon <gorodgorod63@gmail.com>
2019-10-21 14:04:41 +0300

    updated rest example with operations list and order creation

--- examples/rest.py
+++ examples/rest.py
@@ -1,6 +1,51 @@
 from openapi_client import openapi
+from datetime import datetime, timedelta
+from pytz import timezone
 
 token = 'YOUR TOKEN'
 client = openapi.sandbox_api_client(token)
+client.sandbox.sandbox_clear_post()
 client.sandbox.sandbox_register_post()
 client.sandbox.sandbox_currencies_balance_post(sandbox_set_currency_balance_request={"currency": "USD", "balance": 1000})
+
+
+def print_24hr_operations():
+    now = datetime.now(tz=timezone('Europe/Moscow'))
+    yesterday = now - timedelta(days=1)
+    ops = client.operations.operations_get(_from=yesterday.isoformat(), to=now.isoformat())
+    print("operations")
+    print(ops)
+    print()
+
+
+def print_orders():
+    orders = client.orders.orders_get()
+    print("active orders")
+    print(orders)
+    print()
+
+
+def make_order():
+    order_response = client.orders.orders_limit_order_post(figi='BBG009S39JX6',
+                                                           limit_order_request={"lots": 1,
+                                                                                "operation": "Buy",
+                                                                                "price": 0.01})
+    print("make order")
+    print(order_response)
+    print()
+    return order_response
+
+# won't work in sandbox - orders are being instantly executed
+# def cancel_order():
+#     cancellation_result = client.orders.orders_cancel_post(order_id=order_response.payload.order_id)
+#     print("cancel order")
+#     print(cancellation_result)
+#     print()
+
+
+print_24hr_operations()
+print_orders()
+order_response = make_order()
+print_orders()
+# cancel_order()
+# print_orders()
