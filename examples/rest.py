c45579b9fdb3c4e62c45590fb6b2f12321596b2a
awethon <gorodgorod63@gmail.com>
2020-03-02 13:57:14 +0300

    rest example fix

--- examples/rest.py
+++ examples/rest.py
@@ -4,8 +4,8 @@ from pytz import timezone
 
 token = 'YOUR TOKEN'
 client = openapi.sandbox_api_client(token)
-client.sandbox.sandbox_clear_post()
 client.sandbox.sandbox_register_post()
+client.sandbox.sandbox_clear_post()
 client.sandbox.sandbox_currencies_balance_post(sandbox_set_currency_balance_request={"currency": "USD", "balance": 1000})
 
 
